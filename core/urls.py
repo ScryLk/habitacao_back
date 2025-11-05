6e842fa000a6c152cef9b5a637584cecd0f43286
ScryLk <lucaskepler991@gmail.com>
2025-11-05 15:27:47 -0300

    new commiiiiiiiiiiiiiitssssss

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,17 @@ URL configuration for core project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('habitacao.api.urls')),
+    # Compat: redirect /api/ to versioned base /api/v1/
+    path('api/', RedirectView.as_view(url='/api/v1/', permanent=False)),
+    # Optional convenience: redirect root to Swagger
+    path('', RedirectView.as_view(url='/api/v1/swagger/', permanent=False)),
 ]
 
 # Serve media files in development
